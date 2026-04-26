3caae3f9278bedee30f7212e4d969fc6ec42e4b4
Danny <162836923+danncd@users.noreply.github.com>
2026-04-26 00:37:41 -0400

    Added url support

--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -2,18 +2,22 @@ export const revalidate = 3600;
 
 import { getCoursesBySemester, getInstructorSummaries, getSemesters } from "@/utils/server/utils";
 import ScheduleContent from "./components/Content";
+import { Suspense } from "react";
 
 export default async function Schedule() {
-	const semesterData = await getCoursesBySemester();
-	const semesters = await getSemesters();
+    const semesterData = await getCoursesBySemester();
+    const semesters = await getSemesters();
     const instructorSummaries = await getInstructorSummaries();
-	return (
-		<main className="min-h-[calc(100vh-4rem)]">
-			<ScheduleContent 
-                semesters={semesters}
-                semesterData = {semesterData}
-                instructorSummaries={instructorSummaries}
-            />
-		</main>
-	);
+
+    return (
+        <main className="min-h-[calc(100vh-4rem)]">
+            <Suspense fallback={<div className="w-full h-screen animate-pulse bg-gray-100 dark:bg-gray-800/20 rounded-lg" />}>
+                <ScheduleContent 
+                    semesters={semesters}
+                    semesterData={semesterData}
+                    instructorSummaries={instructorSummaries}
+                />
+            </Suspense>
+        </main>
+    );
 }
