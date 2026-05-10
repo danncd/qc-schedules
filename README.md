78cd08acc9af59cea54394d9dafcef94d5ef99a1
Danny <162836923+danncd@users.noreply.github.com>
2026-05-10 19:55:00 -0400

    Suspense added

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>QC Schedules</h1>
+<p>A website listing Queens College current courses and professor data.</p>
--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -1,6 +1,7 @@
 import { manrope } from "@/_lib/fonts";
 import { getInstructorCourseSummaries, getScheduleData } from "@/_utils/server";
 import ScheduleClient from "./_components/ScheduleClient";
+import { Suspense } from "react";
 
 export default async function SchedulePage() {
 	const { semesterData, semesterNames } = await getScheduleData();
@@ -11,7 +12,9 @@ export default async function SchedulePage() {
 			<h1 className={`${manrope.className} font-bold text-xl`}>
 				Course Schedule Lookup
 			</h1>
-			<ScheduleClient semesterData={semesterData} semesterNames={semesterNames} instructorCourseSummary={instructorCourseSummary}/>
+			<Suspense fallback={<div className="w-full h-screen animate-pulse bg-gray-100 dark:bg-gray-800/20 rounded-lg" />}>
+				<ScheduleClient semesterData={semesterData} semesterNames={semesterNames} instructorCourseSummary={instructorCourseSummary}/>
+			</Suspense>
 		</main>
 	);
 }
